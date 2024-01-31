d3165c8df98c2db2f52bfa7610ac636c599f1654
shamsragimova <rahimova070@gmail.com>
2024-01-31 19:34:06 +0400

    Shems changed

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hello java"); //by Azar
+        System.out.println("Hello Matrix"); //by Shems
     }
 }
